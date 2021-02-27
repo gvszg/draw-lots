a6c595be2a84e5b4e44b93f0918c8b1eb8c98e7f
jamesyu <zetaentranceserious@gmail.com>
2021-02-27 11:53:13 +0800

    feat: Guest fill in form data

--- src/main/java/com/project/drawlots/controller/GuestController.java
+++ src/main/java/com/project/drawlots/controller/GuestController.java
@@ -1,26 +1,52 @@
 package com.project.drawlots.controller;
 
+import com.project.drawlots.dto.GuestParamsDTO;
+import com.project.drawlots.model.Guest;
+import com.project.drawlots.service.GuestService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
 @Slf4j
 @Controller
 @RequestMapping(value = {"/guests"})
 public class GuestController {
+    private final GuestService guestService;
+
+    @Autowired
+    public GuestController(GuestService guestService) {
+        this.guestService = guestService;
+    }
 
     @GetMapping(value = {"", "/"})
-    public String index() {
+    public String index(Model model) {
         log.info("Requested Controller - GuestController\nAction - index");
+        model.addAttribute("guest", new GuestParamsDTO());
         return "guests/index";
     }
 
     @PostMapping(value = {"", "/"})
-    public String fillIn(ModelAttribute modelAttribute) {
-        log.info(String.valueOf(modelAttribute));
+    public String fillIn(@ModelAttribute("guest")GuestParamsDTO guestParamsDTO, HttpServletRequest request) {
+
+        try {
+            Map<String, String[]> parameters = request.getParameterMap();
+            String username = parameters.get("guest[username]")[0];
+            int age = Integer.parseInt(parameters.get("guest[age]")[0]);
+            GuestParamsDTO guestParams = new GuestParamsDTO(username, age);
+
+            Guest guest = this.guestService.create(guestParams);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+
         return "guests/thanks";
     }
 }
